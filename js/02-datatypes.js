e6374127072b3ec6a5d5c0c393ab246020eab6cc
xonymous7 <portswig3@gmail.com>
2024-10-17 08:56:21 +0530

    day 3

---
+++ js/02-datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) using node js not browser
+
+let name = "xonymous" //string 
+let age ="20" // number === 2 to the power 53 // greater than number = bigint
+let isloggedin = false // boolean , either true/false
+let state = null       // null = standalone value
+let city                // undefined = variable with no value
+                   //symbol = unique
+
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
