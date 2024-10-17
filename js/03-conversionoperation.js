eddeee59eb66bb191c0ee4f55b3028318fea03c6
xonymous7 <portswig3@gmail.com>
2024-10-17 09:15:34 +0530

    conversion of datatypes

---
+++ js/03-conversionoperation.js
@@ -0,0 +1,26 @@
+let score = "xony"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber)
+console.log(valueinNumber)
+
+// "33" = 33
+// "33abc" = NaN (not a number)
+// true/false = 1/0
+
+let isloggedin = 1
+
+let booleanisloggedin = Boolean(isloggedin)
+
+console.log(booleanisloggedin)
+
+// "" empty string = false
+// defined string = true
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+
+console.log(stringnumber)
