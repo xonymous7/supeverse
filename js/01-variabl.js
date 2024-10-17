58fab084df66d219f790a9d5b6879828e554484e
xonymous7 <portswig3@gmail.com>
2024-10-17 03:03:00 +0000

    day 2

--- js/01-variabl.js
+++ js/01-variabl.js
@@ -1,4 +1,19 @@
-const account-id =1222554
+const accountid =1222554
 let accountemail = "xony@gmail.com"
-var accountpassword- "12345"
-accountcity - "lucknow"
+var accountpassword = "12345"
+accountcity = "lucknow"
+let accountstate  //if no value provided to a variabl , output=undefined
+
+// accountid = 3 // not allowed
+
+accountemail = "hc@hc.com"
+accountpassword = "414141"
+accountcity = "bengaluru"
+
+/*
+NEVER USE VAR CUZ OF BLOCK SCOPE ND FUNCTIONAL SCOPE
+*/
+
+
+console.log(accountid)
+console.table([accountid, accountemail,accountpassword,accountcity,accountstate])
